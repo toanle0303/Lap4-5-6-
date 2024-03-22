2c4239d1ab65a5974da01fb5b8af51cc7f8f35e1
GinLe <162123226+toanle0303@users.noreply.github.com>
2024-03-22 12:31:38 +0700

    Add files via upload

---
+++ lab4_5_6/footer.php
@@ -0,0 +1,12 @@
+</div>
+
+</div>
+</div>
+<div id="wrapper" class="container">
+<footer>
+Comeback <a href="/lab4_5_6/index.php">Home page</a><br />
+&#169; 2022 - Trendemy
+</footer>
+</div>
+</body>
+</html>
